6d8ba7629711911ccf8ec5cdce35caa4dc20569a
Ericson-Manalo <ericmana98@gmail.com>
2024-08-09 19:58:07 +0200

    defined another component called coreconcept and called it into the App()

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactImg from "./assets/react-core-concepts.png";
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
-function randomInt(max){
+function randomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
@@ -22,11 +22,24 @@ function Header() {
   );
 }
 
+function CoreConcept() {
+  return (
+    <li>
+      <img src="" alt="" />
+      <h3></h3>
+      <p></p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header/>
       <main>
+      <section id="core-concepts">
+        <CoreConcept/>
+      </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
